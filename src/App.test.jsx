220c55afa38b11a795f7dfdef3ad3bac0475b7ec
Jeannie Tran <jeanniet13@gmail.com>
2023-05-19 10:23:56 -0700

    add mantine

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,6 @@ import App from './App';
 describe('App', () => {
     it('tests', () => {
         render(<App />);
-        expect(screen.getByText('Vite + React')).toBeInTheDocument();
+        expect(screen.getByText('Character Name')).toBeInTheDocument();
     })
 });
